4827e054f890db72db0cc541d91bb6d05d182a8e
kartikkapadi <163292522+kartikkapadi@users.noreply.github.com>
2026-01-30 11:52:22 +0000

    learning js basic

---
+++ 01_basic/code.js
@@ -0,0 +1,14 @@
+console.log("hi");
+const accountID=" kartikkapadi@gmail.com"
+var  accountpassword=" 12345"
+let accountemail=" kartikkapadi@gmail.com"
+accountcity="pune"
+
+let accountstate; // undefined value 
+
+//accountID=2; // not allowed 
+/*
+prefer not to use var because of issue in block scopre and functional scope 
+ */
+console.log(accountID);
+console.table([accountID,accountemail,accountpassword,accountcity]);
