7c2536cbaf1490629581a047d3f7cc19e7d0c2ec
kartikkapadi <163292522+kartikkapadi@users.noreply.github.com>
2026-01-31 16:36:34 +0000

    conversion of data types

---
+++ 01_basic/02_datatype.js
@@ -0,0 +1,31 @@
+"use strict "; // treat all js code as a newer version 
+
+// alert (3+3) // we are using nodejs , not browser 
+
+console.log(3+3);
+// code readability should be high 
+console.log("kartik");
+
+let name="hitesh"
+let age=23
+let isLoggedIn = false
+let state;
+
+// number => 2 to pwer 53 
+// bigint 
+// String => " "
+// boolean => true/false 
+// null => standalone value 
+// undefined => abhi value assgin nahi uhi hai 
+// symbol => unique 
+
+
+// object   
+
+console.log(typeof " hitesh");
+console.log (typeof 123);
+console.log(typeof 'a');
+console.log(typeof 24.5);
+console.log(typeof true);
+console.log(typeof null); // type iska object hai 
+console.log(typeof undefined) //undefined he hai 
