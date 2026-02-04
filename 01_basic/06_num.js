b80a33f80cb8309366b016f3cc0930128dc24b8c
kartikkapadi <163292522+kartikkapadi@users.noreply.github.com>
2026-02-04 12:02:28 +0000

    number learn

---
+++ 01_basic/06_num.js
@@ -0,0 +1,24 @@
+const score =400
+const balance =new Number(100) // dediacte that we need compulsaory number 
+console.log(balance);
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+console.log(score);
+
+const othernumber=23.8966
+console.log(othernumber.toPrecision(4));
+
+const hundred=1000000
+console.log(hundred.toLocaleString('en-in'));
+console.log(Number.MAX_VALUE);
+console.log(Number.MAX_SAFE_INTEGER);
+console.log(Number.MIN_VALUE);
+
+//  ++++++++++++++++++++++++++++++maths+++++++++++++++++++++++++++++++++++  //
+ console.log(Math); // internally object he hai 
+ console.log (Math.abs(-4)); // convert - value into a + value 
+ console.log(Math.round(4.6));
+ console.log(Math.ceil(4.2));
+ console.log(Math.floor(4.2));
+ console.log(Math.min(4,5,6,3,7,9));
+ console.log(Math.m(4,5,6,3,7,9));
